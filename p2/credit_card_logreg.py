5b180f3c7b682d3ba582649f611922469f373020
hglad <hanseglad@gmail.com>
2019-10-14 15:49:33 +0200

    Update credit_card_logreg.py

--- p2/credit_card_logreg.py
+++ p2/credit_card_logreg.py
@@ -1,6 +1,11 @@
 import numpy as np
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.metrics import confusion_matrix, accuracy_score, roc_auc_score
+
 # Read file and create dataframe
 df = pd.read_excel('default of credit card clients.xls', header=1, skiprows=0, index_col=0, na_values={})
 df.rename(index=str, columns={"default payment next month": "defaultPaymentNextMonth"}, inplace=True)
@@ -13,4 +18,12 @@ y = df.loc[:, df.columns == 'defaultPaymentNextMonth'].values
 print (df.head())
 ncols = X.shape[1]
 
-onehot = np.zeros(ncols)
+# Categorical variables to one-hot's
+onehotencoder = OneHotEncoder(categories="auto")
+
+X = ColumnTransformer(
+    [("", onehotencoder, [3]),],
+    remainder="passthrough"
+).fit_transform(X)
+
+print (X)

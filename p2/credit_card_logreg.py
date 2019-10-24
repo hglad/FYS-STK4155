gradient descent fixed

p2/credit_card_logreg.py
@@ -1,26 +1,70 @@
 from functions import *
 
-def main():
-    # Read file and create dataframe
-    df = pd.read_excel('default of credit card clients.xls', header=1, skiprows=0, index_col=0, na_values={})
-    df.rename(index=str, columns={"default payment next month": "defaultPaymentNextMonth"}, inplace=True)
+def main(dataset=0):
+    if dataset == 0: # credit card data
+        # Read file and create dataframe
+        df = pd.read_excel('default of credit card clients.xls', header=1, skiprows=0, index_col=0, na_values={})
+        df.rename(index=str, columns={"default payment next month": "defaultPaymentNextMonth"}, inplace=True)
 
-    # Create matrix X of explanatory variables (23 features)
-    X = df.loc[:, df.columns != 'defaultPaymentNextMonth'].values
-    # target variable: if customer defaults or not
-    y = df.loc[:, df.columns == 'defaultPaymentNextMonth'].values
-    y = y[:,0]
+        df = df.drop(df[(df.BILL_AMT1 == 0)&
+                    (df.BILL_AMT2 == 0)&
+                    (df.BILL_AMT3 == 0)&
+                    (df.BILL_AMT4 == 0)&
+                    (df.BILL_AMT5 == 0)&
+                    (df.BILL_AMT6 == 0)].index)
+        df = df.drop(df[(df.PAY_AMT1 == 0)&
+                    (df.PAY_AMT2 == 0)&
+                    (df.PAY_AMT3 == 0)&
+                    (df.PAY_AMT4 == 0)&
+                    (df.PAY_AMT5 == 0)&
+                    (df.PAY_AMT6 == 0)].index)
 
-    print (df.head())
-    ncols = X.shape[1]
+        # Create matrix X of explanatory variables (23 features)
+        X = df.loc[:, df.columns != 'defaultPaymentNextMonth'].values
+        # target variable: if customer defaults or not
+        y = df.loc[:, df.columns == 'defaultPaymentNextMonth'].values
 
-    # Categorical variables to one-hots
-    onehotencoder = OneHotEncoder(categories="auto")
+        print (df.head())
 
-    X = ColumnTransformer(
-        [("", onehotencoder, [3]),],
-        remainder="passthrough"
-    ).fit_transform(X)
+
+        # Categorical variables to one-hots
+        onehotencoder = OneHotEncoder(categories="auto", sparse=False)
+
+        X = ColumnTransformer(
+            [("", onehotencoder, [1,2,3,5,6,7,8,9]),],
+            remainder="passthrough"
+        ).fit_transform(X)
+
+        scaler = StandardScaler(with_mean=False)
+        X = scaler.fit_transform(X)
+
+        y_onehot = onehotencoder.fit_transform(y)
+
+    if dataset == 1: # exam marks (towards data science)
+        infile = open('marks.txt', 'r')
+        n = 0
+        for line in infile:
+            n += 1
+
+        X = np.ones((n,3))
+        y = np.zeros(n)
+
+        i = 0
+        infile = open('marks.txt', 'r')
+        for line in infile:
+            l = line.split(',')
+            X[i,1], X[i,2], y[i] = l[0], l[1], l[2]
+            i += 1
+
+    if dataset == 2: # breast cancer data
+        from sklearn.datasets import load_breast_cancer
+        data = load_breast_cancer()
+        X = data.data
+        y = data.target
+
+        scaler = StandardScaler()
+        X = scaler.fit_transform(X)
+
 
     print (X)
     # Split into train and test data
@@ -29,36 +73,16 @@ def main():
     # logreg_sklearn(X_train, X_test, y_train, y_test)
     my_logreg(X_train, X_test, y_train, y_test)
 
-def main2():
-    infile = open('marks.txt', 'r')
-    n = 0
-    for line in infile:
-        n += 1
-
-    X = np.ones((n,3))
-    y = np.zeros(n)
-
-    i = 0
-    infile = open('marks.txt', 'r')
-    for line in infile:
-        split = line.split(',')
-        X[i,1], X[i,2], y[i] = split[0], split[1], split[2]
-        i += 1
-
-
-    # Split into train and test data
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=123)
-
-    my_logreg(X_train, X_test, y_train, y_test)
-    # logreg_sklearn(X_train, X_test, y_train, y_test)
 
 if __name__ == '__main__':
-    main2()
+    main(0)
 
 # marks.txt data: 100 % accuracy, random_state=123, test_size=0.3
-    # iters = 50000
-    # gamma = 5e-2
-    # beta_0 = np.random.uniform(-10000,10000,m)         # random initial weights
+# iters = 50000
+# gamma = 5e-2
+# beta_0 = np.random.uniform(-10000,10000,m)         # random initial weights
+
+
 
 
 

p2/functions.py
@@ -51,10 +51,13 @@ def total_cost(n, y, p, beta):
 
 
 def gradient(m, x, y, beta):
-    exp = np.exp( prob(x,beta) )
-    p = exp/(1+exp)#*(1 - exp/(1+exp))
-    return -np.dot(x.T, (y - p))
+    # exp = np.exp( prob(x,beta) )
+    # exp = np.exp(x @ beta)
+    p = 1./(1+np.exp(-x@beta))
+    # p = 1./(1+exp)
+    # p = exp/(1+exp)#*(1 - exp/(1+exp))
 
+    return np.dot(x.T, (p - y))
     # return (1/m) * np.dot(x.T, prob(x,beta) - y)
 
 
@@ -70,11 +73,11 @@ def gradient_descent(x, beta, y, iters=100, gamma=1e-2):
 
         # if (abs(norm) < 1e-12):
         #     gamma *= 1.01
-        if (abs(norm) > 0):
-            gamma *= 0.99
+        # if (abs(norm) > 0):
+        #     gamma *= 0.99
 
         beta = new_beta
-        print (norm)
+        # print (norm)
         if (norm < 1e-10):
             print (norm, gamma)
             return beta, norm
@@ -90,23 +93,25 @@ def my_logreg(X_train, X_test, y_train, y_test):
     # X[samples, features]
     n = X_train.shape[0]                    # number of training samples
     m = X_train.shape[1]                    # number of features
-    iters = 100000
-    gamma = 1e-8
-    beta_0 = np.random.uniform(-10000,10000,m)         # random initial weights
+    # m = len(X[0])
+    iters = 1000
+    # gamma = 1e-8
+    # beta_0 = np.random.uniform(-10000,10000,m)         # random initial weights
     # opt_beta, norm = gradient_descent(X_train, beta_0, y_train, iters, gamma)
 
-    params = np.logspace(np.log10(1e-5), np.log10(1e-4), 1)
+    params = np.logspace(np.log10(1e-3), np.log10(1e-4), 1)
 
     for gamma in params:
-        beta_0 = np.random.uniform(-1,1,m)
+        beta_0 = np.random.uniform(-10,10,m)
+        beta_0 = np.reshape(beta_0, (m,1))
         opt_beta, norm = gradient_descent(X_train, beta_0, y_train, iters=iters, gamma=gamma)
 
         # Predict using optimal weights
         print ("Initial beta:", beta_0)
         print ("Optimal beta:", opt_beta)
 
-        predict = prob(X_test, opt_beta)     # values between 0 and 1
-        y_pred = (predict >= 0.5).astype(int)
+        predict = prob(X_test, opt_beta)      # values between 0 and 1
+        y_pred = (predict >= 0.5).astype(int) # convert to 0 or 1
         accuracy = np.mean(y_pred == y_test)
         diff = y_test - y_pred
 
@@ -128,6 +133,7 @@ def my_logreg(X_train, X_test, y_train, y_test):
         #
         # plt.plot(y_pred, '-bo')
         # plt.show()
+
 
 
 